a79b974d939498252e7fd0d5f98616d68ce6f309
x10an14 <x10an14@gmail.com>
2017-04-28 22:01:36 +0200

    Enhancement: Remove unused imports from manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 # PyPi imports:
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
